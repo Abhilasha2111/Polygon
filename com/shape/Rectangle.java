ecd454fc509dc2eff6271a54ac9bd191cff7f40a
Abhilasha2111 <abhi211197@gmail.com>
2022-08-31 08:06:43 +0530

    polygon inserted

---
+++ com/shape/Rectangle.java
@@ -0,0 +1,20 @@
+package com.shape;
+public class Rectangle implements Polygon{
+	float length=4,breadth=5;
+	
+
+	@Override
+	public void calcArea() {
+		float area =length*breadth; 
+		System.out.println("Rectangle of Area :"+area);
+		
+	}
+
+	@Override
+	public void calcPeri() {
+		float perimeter =  2*(length+breadth);
+		System.out.println("Perimeter of rectangle :"+perimeter);
+	}
+	
+}
+
---
+++ com/shape/Square.java
@@ -0,0 +1,17 @@
+package com.shape;
+public class Square implements Polygon {
+	float side=5;
+
+	@Override
+	public void calcArea() {
+		float area = side*side;
+		System.out.println("Area Of Square "+area);
+	}	
+
+	@Override
+	public void calcPeri() {
+		float perimeter = 4 * side;
+		System.out.println("Perimeter of Square"+perimeter);
+		
+	}
+}
